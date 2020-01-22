23a4194dc8706d2939a498df21622f932716fd27
Nick Wardwell <nick4848@hotmail.com>
2020-01-22 08:36:44 -0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+window.axios = require('axios/dist/axios.min.js');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import 'jquery-dropdown-datepicker';
